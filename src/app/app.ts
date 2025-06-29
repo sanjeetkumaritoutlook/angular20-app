1e96049aab4fb2e4909b3ffdad58f53ba9528c97
sanjeetkumaritoutlook-user <sanjeetkumarit@outlook.com>
2025-06-29 19:53:49 +0530

    ghh

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,9 +4,10 @@ import {Product} from './product/product';
 import { Rte } from './rte/rte';
 import { Porsche } from './porsche/porsche';
 import { ChineseDs } from './chinese-ds/chinese-ds';
+import { Modal } from './modal/modal';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Product,Rte,Porsche,ChineseDs],
+  imports: [RouterOutlet,Product,Rte,Porsche,ChineseDs,Modal],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
